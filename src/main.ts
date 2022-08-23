e135ae6e46f16be07240e6e7dc027f5b2d9fe6a1
Sylent-Sys <renaldiapriyanto419@gmail.com>
2022-08-23 10:50:44 +0700

    add firebase library and pdfmake

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import type { Interaction, Message } from 'discord.js'
 import { IntentsBitField } from 'discord.js'
 import { Client } from 'discordx'
 import { randomUUID } from 'crypto'
-
+import { initializeApp, cert } from 'firebase-admin/app'
+import fs from 'fs'
+import path from 'path'
 export const bot = new Client({
   botId: process.env.BOT_ID ?? randomUUID(),
   botGuilds: [(client) => client.guilds.cache.map((guild) => guild.id)],
@@ -39,6 +41,13 @@ bot.on('messageCreate', (message: Message) => {
 })
 
 async function run() {
+  initializeApp({
+    projectId: 'renka-bot',
+    credential: cert(
+      path.resolve(fs.realpathSync('.'), 'renka-bot-firebase-adminsdk.json')
+    ),
+    storageBucket: 'renka-bot.appspot.com',
+  })
   await importx(dirname(import.meta.url) + '/{events,commands}/**/*.{ts,js}')
   if (!process.env.BOT_TOKEN) {
     throw Error('Could not find BOT_TOKEN in your environment')
